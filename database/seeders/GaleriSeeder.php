9f72d3c0736c9d6ca21977d739056d9696b4ec7c
yoshitri29 <yossietriwinanda29@gmail.com>
2024-12-08 08:52:18 -0800

    update galeri

--- database/seeders/GaleriSeeder.php
+++ database/seeders/GaleriSeeder.php
@@ -10,15 +10,79 @@ class GaleriSeeder extends Seeder
     public function run()
     {
         Galeri::create([
-            'title' => 'Contoh Gambar 1',
-            'image_path' => 'galeri/contoh1.jpg',
+            'title' => ' Gambar 1',
+            'image_path' => 'galeri/1.jpg',
         ]);
 
         Galeri::create([
-            'title' => 'Contoh Gambar 2',
-            'image_path' => 'galeri/contoh2.jpg',
+            'title' => ' Gambar 2',
+            'image_path' => 'galeri/2.jpg',
         ]);
 
-        // Tambahkan lebih banyak gambar sesuai kebutuhan
+        // Menambahkan 10 gambar tambahan
+        Galeri::create([
+            'title' => ' Gambar 3',
+            'image_path' => 'galeri/3.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 4',
+            'image_path' => 'galeri/4.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 5',
+            'image_path' => 'galeri/5.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 6',
+            'image_path' => 'galeri/6.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 7',
+            'image_path' => 'galeri/7.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 8',
+            'image_path' => 'galeri/8.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 9',
+            'image_path' => 'galeri/9.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 10',
+            'image_path' => 'galeri/10.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 11',
+            'image_path' => 'galeri/11.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 12',
+            'image_path' => 'galeri/12.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 13',
+            'image_path' => 'galeri/13.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 14',
+            'image_path' => 'galeri/14.jpg',
+        ]);
+
+        Galeri::create([
+            'title' => ' Gambar 15',
+            'image_path' => 'galeri/15.jpg',
+        ]);
     }
 }
